ed83a9cd2f94dcadb936287594be54f3d05b93e8
DerThorsten <derthorstenbeier@gmail.com>
2022-06-22 11:25:28 +0200

    chaging widgets

--- src/napari_splineit2/_widget.py
+++ src/napari_splineit2/_widget.py
@@ -6,11 +6,14 @@ see: https://napari.org/plugins/stable/npe2_manifest_specification.html
 
 Replace code below according to your needs.
 """
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLineEdit
 from magicgui import magic_factory
 
+from .layer.layer_factory import interpolation_factory
 
-class ExampleQWidget(QWidget):
+
+
+class SplineitQWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -19,14 +22,21 @@ class ExampleQWidget(QWidget):
         super().__init__()
         self.viewer = napari_viewer
 
-        btn = QPushButton("Click me!")
+
+        self.layer_name_edit = QLineEdit("Splines")
+
+        btn = QPushButton("Click me!!!!")
         btn.clicked.connect(self._on_click)
 
+
+
         self.setLayout(QHBoxLayout())
+        self.layout().addWidget(self.layer_name_edit)
         self.layout().addWidget(btn)
 
     def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        base_name = self.layer_name_edit.text()
+        interpolation_factory(self.viewer, ctrl_layer_name=f"{base_name}-CTRL", interpolated_layer_name=f"{base_name}-Interpolated")
 
 
 @magic_factory
--- src/napari_splineit2/layer/layer_factory.py
+++ src/napari_splineit2/layer/layer_factory.py
@@ -2,12 +2,12 @@ from . _shape_list import CtrlLayerShapeList
 from . _interpolated_layer import InterpolatedLayer
 from . _ctrl_layer import CtrlPtrLayer
 
-def interpolation_factory(viewer):
+def interpolation_factory(viewer, ctrl_layer_name="CtrLayer", interpolated_layer_name="Interpolated"):
 
-    interpolated_layer = InterpolatedLayer(name="Interpolated")
+    interpolated_layer = InterpolatedLayer(name=interpolated_layer_name)
     viewer.add_layer(interpolated_layer)
 
-    ctrl_layer = CtrlPtrLayer(name="CtrLayer", interpolated_layer=interpolated_layer)
+    ctrl_layer = CtrlPtrLayer(name=ctrl_layer_name, interpolated_layer=interpolated_layer)
     viewer.add_layer(ctrl_layer)
 
     return interpolated_layer, ctrl_layer
